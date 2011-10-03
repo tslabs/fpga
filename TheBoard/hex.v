1c81774427d79816fb6e40ad3e0f01b3f5df493d
at2313 <at2313@localhost>
2011-10-03 16:43:22 +0000

    Hex added

---
+++ TheBoard/hex.v
@@ -0,0 +1,44 @@
+
+module hex	(
+input	        	clk,
+input	        	en,
+input	    [3:0]	val,
+input	    [1:0]	dig,
+output reg	[27:0]	seg
+);
+
+
+always @(posedge clk)
+    if (en)
+    case(dig)
+    2'h0: seg[ 6: 0] <= led;
+    2'h1: seg[12: 7] <= led;
+    2'h2: seg[18:13] <= led;
+    2'h3: seg[27:19] <= led;
+    endcase
+
+
+reg [6:0]   led;
+
+always @*
+	case(val)
+	4'h0: led = 7'b1000000;
+	4'h1: led = 7'b1111001;	
+	4'h2: led = 7'b0100100; 
+	4'h3: led = 7'b0110000; 
+	4'h4: led = 7'b0011001; 
+	4'h5: led = 7'b0010010; 
+	4'h6: led = 7'b0000010; 
+	4'h7: led = 7'b1111000; 
+	4'h8: led = 7'b0000000; 
+	4'h9: led = 7'b0011000; 
+	4'hA: led = 7'b0001000;
+	4'hB: led = 7'b0000011;
+	4'hC: led = 7'b1000110;
+	4'hD: led = 7'b0100001;
+	4'hE: led = 7'b0000110;
+	4'hF: led = 7'b0001110;
+	endcase
+
+
+endmodule
--- TheBoard/top.v
+++ TheBoard/top.v
@@ -2,92 +2,105 @@ module top
 (
 
 // Clocks
-input	[1:0]	CLOCK_24,				//	24 MHz
-input	[1:0]	CLOCK_27,				//	27 MHz
-input			CLOCK_50,				//	50 MHz
-input			EXT_CLOCK,				//	External Clock
+input	[1:0]	clock_24,				//	24 MHz
+input	[1:0]	clock_27,				//	27 MHz
+input			clock_50,				//	50 MHz
+input			ext_clock,				//	External Clock
 // Push Button
-input	[3:0]	KEY,					//	Pushbutton[3:0]
+input	[3:0]	key,					//	Pushbutton[3:0]
 // DPDT Switch
-input	[9:0]	SW,						//	Toggle Switch[9:0]
+input	[9:0]	sw,						//	Toggle Switch[9:0]
 // 7-SEG Display
-output	[6:0]	HEX0,					//	Seven Segment Digit 0
-output	[6:0]	HEX1,					//	Seven Segment Digit 1
-output	[6:0]	HEX2,					//	Seven Segment Digit 2
-output	[6:0]	HEX3,					//	Seven Segment Digit 3
+output	[6:0]	hex0,					//	Seven Segment Digit 0
+output	[6:0]	hex1,					//	Seven Segment Digit 1
+output	[6:0]	hex2,					//	Seven Segment Digit 2
+output	[6:0]	hex3,					//	Seven Segment Digit 3
 // LED
-output	[7:0]	LEDG,					//	LED Green[7:0]
-output	[9:0]	LEDR,					//	LED Red[9:0]
+output	[7:0]	ledg,					//	LED Green[7:0]
+output	[9:0]	ledr,					//	LED Red[9:0]
 // UART
-output			UART_TXD,				//	UART Transmitter
-input			UART_RXD,				//	UART Receiver
+output			uart_txd,				//	UART Transmitter
+input			uart_rxd,				//	UART Receiver
 // SDRAM
-inout	[15:0]	DRAM_DQ,				//	SDRAM Data bus 16 Bits
-output	[11:0]	DRAM_ADDR,				//	SDRAM Address bus 12 Bits
-output			DRAM_LDQM,				//	SDRAM Low-byte Data Mask 
-output			DRAM_UDQM,				//	SDRAM High-byte Data Mask
-output			DRAM_WE_N,				//	SDRAM Write Enable
-output			DRAM_CAS_N,				//	SDRAM Column Address Strobe
-output			DRAM_RAS_N,				//	SDRAM Row Address Strobe
-output			DRAM_CS_N,				//	SDRAM Chip Select
-output			DRAM_BA_0,				//	SDRAM Bank Address 0
-output			DRAM_BA_1,				//	SDRAM Bank Address 0
-output			DRAM_CLK,				//	SDRAM Clock
-output			DRAM_CKE,				//	SDRAM Clock Enable
+inout	[15:0]	dram_dq,				//	SDRAM Data bus 16 Bits
+output	[11:0]	dram_addr,				//	SDRAM Address bus 12 Bits
+output			dram_ldqm,				//	SDRAM Low-byte Data Mask 
+output			dram_udqm,				//	SDRAM High-byte Data Mask
+output			dram_we_n,				//	SDRAM Write Enable
+output			dram_cas_n,				//	SDRAM Column Address Strobe
+output			dram_ras_n,				//	SDRAM Row Address Strobe
+output			dram_cs_n,				//	SDRAM Chip Select
+output			dram_ba_0,				//	SDRAM Bank Address 0
+output			dram_ba_1,				//	SDRAM Bank Address 0
+output			dram_clk,				//	SDRAM Clock
+output			dram_cke,				//	SDRAM Clock Enable
 // Flash
-inout	[7:0]	FL_DQ,					//	FLASH Data bus 8 Bits
-output	[21:0]	FL_ADDR,				//	FLASH Address bus 22 Bits
-output			FL_WE_N,				//	FLASH Write Enable
-output			FL_RST_N,				//	FLASH Reset
-output			FL_OE_N,				//	FLASH Output Enable
-output			FL_CE_N,				//	FLASH Chip Enable
+inout	[7:0]	fl_dq,					//	FLASH Data bus 8 Bits
+output	[21:0]	fl_addr,				//	FLASH Address bus 22 Bits
+output			fl_we_n,				//	FLASH Write Enable
+output			fl_rst_n,				//	FLASH Reset
+output			fl_oe_n,				//	FLASH Output Enable
+output			fl_ce_n,				//	FLASH Chip Enable
 // SRAM
-inout	[15:0]	SRAM_DQ,				//	SRAM Data bus 16 Bits
-output	[17:0]	SRAM_ADDR,				//	SRAM Address bus 18 Bits
-output			SRAM_UB_N,				//	SRAM High-byte Data Mask 
-output			SRAM_LB_N,				//	SRAM Low-byte Data Mask 
-output			SRAM_WE_N,				//	SRAM Write Enable
-output			SRAM_CE_N,				//	SRAM Chip Enable
-output			SRAM_OE_N,				//	SRAM Output Enable
+inout	[15:0]	sram_dq,				//	SRAM Data bus 16 Bits
+output	[17:0]	sram_addr,				//	SRAM Address bus 18 Bits
+output			sram_ub_n,				//	SRAM High-byte Data Mask 
+output			sram_lb_n,				//	SRAM Low-byte Data Mask 
+output			sram_we_n,				//	SRAM Write Enable
+output			sram_ce_n,				//	SRAM Chip Enable
+output			sram_oe_n,				//	SRAM Output Enable
 // SD Card
-output			SD_CS_N,	    		//	SD Card Chip Select
-output			SD_CLK,					//	SD Card Clock
-output			SD_DO,					//	SD Card Data Out
-input			SD_DI,					//	SD Card Data In
+output			sd_cs_n,	    		//	SD Card Chip Select
+output			sd_clk,					//	SD Card Clock
+output			sd_do,					//	SD Card Data Out
+input			sd_di,					//	SD Card Data In
 // I2C
-inout			I2C_SDAT,				//	I2C Data
-output			I2C_SCLK,				//	I2C Clock
+inout			i2c_sdat,				//	I2C Data
+output			i2c_sclk,				//	I2C Clock
 // PS/2
-input		 	PS2_DAT,				//	PS2 Data
-input			PS2_CLK,				//	PS2 Clock
+input		 	ps2_dat,				//	PS2 Data
+input			ps2_clk,				//	PS2 Clock
 // USB JTAG
-input  			TDI,					// CPLD -> FPGA (data in)
-input  			TCK,					// CPLD -> FPGA (clk)
-input  			TCS,					// CPLD -> FPGA (CS)
-output 			TDO,					// FPGA -> CPLD (data out)
+input  			tdi,					// CPLD -> FPGA (data in)
+input  			tck,					// CPLD -> FPGA (clk)
+input  			tcs,					// CPLD -> FPGA (CS)
+output 			tdo,					// FPGA -> CPLD (data out)
 // VGA
-output			VGA_HS,					//	VGA H_SYNC
-output			VGA_VS,					//	VGA V_SYNC
-output	[3:0]	VGA_R,   				//	VGA Red[3:0]
-output	[3:0]	VGA_G,	 				//	VGA Green[3:0]
-output	[3:0]	VGA_B,   				//	VGA Blue[3:0]
+output			vga_hs,					//	VGA H_SYNC
+output			vga_vs,					//	VGA V_SYNC
+output	[3:0]	vga_r,   				//	VGA Red[3:0]
+output	[3:0]	vga_g,	 				//	VGA Green[3:0]
+output	[3:0]	vga_b,   				//	VGA Blue[3:0]
 // Audio CODEC
-output			AUD_ADCLRCK,			//	Audio CODEC ADC LR Clock
-input			AUD_ADCDAT,				//	Audio CODEC ADC Data
-output			AUD_DACLRCK,			//	Audio CODEC DAC LR Clock
-output			AUD_DACDAT,				//	Audio CODEC DAC Data
-inout			AUD_BCLK,				//	Audio CODEC Bit-Stream Clock
-output			AUD_XCK,				//	Audio CODEC Chip Clock
+output			aud_adclrck,			//	Audio CODEC ADC LR Clock
+input			aud_adcdat,				//	Audio CODEC ADC Data
+output			aud_daclrck,			//	Audio CODEC DAC LR Clock
+output			aud_dacdat,				//	Audio CODEC DAC Data
+inout			aud_bclk,				//	Audio CODEC Bit-Stream Clock
+output			aud_xck,				//	Audio CODEC Chip Clock
 // GPIO
-inout	[35:0]	GPIO_0,					//	GPIO Connection 0
-inout	[35:0]	GPIO_1					//	GPIO Connection 1
+inout	[35:0]	gpio_0,					//	GPIO Connection 0
+inout	[35:0]	gpio_1					//	GPIO Connection 1
 );
 
 
+wire        hex_en  = 0;
+wire [3:0]  hex_val = 0;
+wire [1:0]  hex_dig = 0;
+
+hex hex	(
+    .clk     ( clk108  ),
+    .en      ( hex_en  ),
+    .val     ( hex_val ),
+    .dig     ( hex_dig ),
+    .seg     ( {hex3, hex2, hex1, hex0} )
+    );
+
+
 pll	pll (
-	.inclk0 ( CLOCK_24[0] ),
-	.c0 ( clk108 ),
-	.c1 ( clk166 )
+	.inclk0 ( clock_24[0] ),
+	.c0     ( clk108      ),
+	.c1     ( clk166      )
 	);
 
 
