2b6a9cc2479c19dc543d2c326fcf3a53a8dd862c
at2313 <at2313@localhost>
2011-10-12 02:36:52 +0000

    TheBoard: DAP re-factored - all transferred to clock FSM (double-part FSM - DIE IN HELL!!!)

--- TheBoard/avr_dap.v
+++ TheBoard/avr_dap.v
@@ -9,19 +9,20 @@ module avr_dap(
 	output reg  [23:0]	addr,
 	output wire [15:0]	d_wr,
 	input  wire [15:0]	d_rd,
-	output reg			w_rq,
-	output reg			r_rq,
+	output wire			w_rq,
+	output wire			r_rq,
 	input  wire			rq_ack
 	
 );
 
-	assign dap_data = !dap_re_n && !dap_ce_n ? addr[7:0] : 8'hZZ;
+	assign dap_data = !dap_re_n && !dap_ce_n ? dout : 8'hZZ;
 	assign d_wr = {din, din0};
 	assign dap_r_n_b = 1;
 
 	reg [ 7:0]	din;
 	reg [ 7:0]	din0;
 	reg [ 7:0]	dout;
+	reg [ 7:0]	dout0;
 
 
 // strobing input data
@@ -44,168 +45,112 @@ module avr_dap(
 
 
 // FSM
-	localparam MS_IDL = 5'd0;
-	localparam MS_AW1 = 5'd1;
-	localparam MS_AW2 = 5'd2;
-	localparam MS_AW3 = 5'd3;
-	localparam MS_AW4 = 5'd4;
-	localparam MS_AW5 = 5'd5;
-	localparam MS_AW6 = 5'd6;
-	localparam MS_AW7 = 5'd7;
-	localparam MS_AW8 = 5'd8;
-	localparam MS_FRK = 5'd9;
-	localparam MS_DW1 = 5'd10;
-	localparam MS_DW2 = 5'd11;
-	localparam MS_DW3 = 5'd12;
-	localparam MS_DW4 = 5'd13;
-	localparam MS_DW5 = 5'd14;
-	localparam MS_DW6 = 5'd15;
-	localparam MS_DR1 = 5'd16;
-	localparam MS_DR2 = 5'd17;
-	localparam MS_DR3 = 5'd18;
-	localparam MS_DR4 = 5'd19;
-	localparam MS_DR5 = 5'd20;
-	localparam MS_DR6 = 5'd21;
+	localparam IDL = 5'd0;
+	localparam AW1 = 5'd1;
+	localparam AW2 = 5'd2;
+	localparam AW3 = 5'd3;
+	localparam AW4 = 5'd4;
+	localparam AW5 = 5'd5;
+	localparam ROW = 5'd6;
+	localparam DW1 = 5'd7;
+	localparam DW2 = 5'd8;
+	localparam DW3 = 5'd9;
+	localparam DR1 = 5'd10;
+	localparam DR2 = 5'd11;
+	localparam DR3 = 5'd12;
 	
+// some control signals derived from state
+	assign w_rq = (ms == DW3) && !we && !rq_ack;
+	assign r_rq = (ms == ROW) && re && !rq_ack;
 	
-// next state clocking
 	reg [4:0]	ms;
-	reg [4:0]	ms_next;
 	
 	always @(posedge clk)
-	ms <= ms_next;
-	
-
-// states assigning
-
-// CEn	|-----______|_________|____|_______|______|____|______|________|_____|_____|__-----
-// WEn	|-----------|_________|----|-------|______|----|------|________|-----|-----|-------
-// MS	|IDLE       |AW1	  |AW2 |AW3    |AW4	  |AW5 |AW6   |AW7     |AW8  |AW9  |IDLE
-// ADDR	|					  | 7:0               |15:8		 		  |23:16
-
-// CEn	  |-----______|_________|____|_______|_______|__________|______|________|____|_____|__-----
-// WEn	  |-----------|_________|----|-------|_______|----------|------|________|----|-----|-------
-// MS	  |IDLE       |DW1	    |DW2 |DW3    |DW4    |DW5       |DW6   |DW1     |DW2 |DW9  |IDLE
-// DATA	  |	                    |7:0			     |15:8      	            |7:0
-// W_RQ   |__________________________________________|----------|__________________________________        
-// RQ_ACK |____________________________________________________-|-_________________________________ 
-// ADDR   |ADDR                                                     |ADDR+1
-
-// CEn	  |-----______|______|____|_______|_______|_______|______|____|_______
-// REn	  |-----------|______|____|-------|_______|-------|______|____|-------
-// MS	  |IDLE       |DR1   |DR2 |DR3    |DR4    |DR5    |DR1	|DR2 |DR3   
-// DATA	  |	                 |7:0	      |15:8                  |7:0	     
-// R_RQ   |___________|------|____________________________|------|____________      
-// RQ_ACK |_________________-|-_________________________________-|-___________
-// ADDR   |ADDR              |ADDR+1                                                
-
-// On any wait state if CE is deasserted FSM returns to IDLE state
-
-	always @*
+	begin
 	if (!ce)
-		ms_next = MS_IDL;
+		ms <= IDL;
 	else
 	case (ms)
-MS_IDL:
-		ms_next = we ? MS_AW1 : MS_IDL;
-
-MS_AW1:	// wait for end of WEn
-		ms_next = we ? MS_AW1 : MS_AW2;
-MS_AW2:	// fetch addr[7:0]
-		ms_next = MS_AW3;
-MS_AW3:	// wait for start of WEn
-		ms_next = we ? MS_AW4 : MS_AW3;
-MS_AW4:	// wait for end of WEn
-		ms_next = we ? MS_AW4 : MS_AW5;
-MS_AW5:	// fetch addr[15:8]
-		ms_next = MS_AW6;
-MS_AW6:	// wait for start of WEn
-		ms_next = we ? MS_AW7 : MS_AW6;
-MS_AW7:	// wait for end of WEn
-		ms_next = we ? MS_AW7 : MS_AW8;
-MS_AW8:	// fetch addr[23:16]
-		ms_next = MS_FRK;
-
-MS_FRK:	// wait for REn or WEn
+IDL:	// wait for 1st WEn
 		if (we)
-			ms_next = MS_DW1;
+			ms <= AW1;
+
+AW1:	// if end of WEn, fetch addr[7:0]
+		if (!we)
+		begin
+			addr[7:0] <= din;
+			ms <= AW2;
+		end
+		
+AW2:	// wait for 2nd WEn
+		if (we)
+			ms <= AW3;
+		
+AW3:	// if end of WEn, fetch addr[15:8]
+		if (!we)
+		begin
+			addr[15:8] <= din;
+			ms <= AW4;
+		end
+		
+AW4:	// wait for 3rd WEn
+		if (we)
+			ms <= AW5;
+		
+AW5:	// if end of WEn, fetch addr[23:16]
+		if (!we)
+		begin
+			addr[23:16] <= din;
+			ms <= ROW;
+		end
+		
+ROW:	// wait for REn or WEn
+		if (we)
+			ms <= DW1;
 		else
+		if (re && rq_ack)
+		begin
+			{dout0, dout} <= addr;			//DEBUG!!!
+			// {dout0, dout} <= d_rd;
+			addr <= addr + 1;
+			ms <= DR1;
+		end
+
+DW1:	// if end of WEn, fetch data[7:0]
+		if (!we)
+		begin
+			din0 <= din;
+			ms <= DW2;
+		end
+
+DW2:	// wait for 2nd WEn
+		if (we)
+			ms <= DW3;
+		
+DW3:	// if end of WEn, write request appears, waiting for request acknowledge, repeat from ROW
+		if (!we && rq_ack)
+		begin
+			addr <= addr + 1;
+			ms <= ROW;
+		end
+
+DR1:	// wait for end of 1st REn
+		if (!re)
+		begin
+			dout <= dout0;
+			ms <= DR2;
+		end
+
+DR2:
 		if (re)
-			ms_next = MS_DR1;
-		else
-			ms_next = MS_FRK;
+			ms <= DR3;
 
-MS_DW1:	// wait for end of WEn
-		ms_next = we ? MS_DW1 : MS_DW2;
-MS_DW2:	// fetch data[7:0]
-		ms_next = MS_DW3;
-MS_DW3:	// wait for start of WEn
-		ms_next = we ? MS_DW4 : MS_DW3;
-MS_DW4:	// wait for end of WEn
-		ms_next = we ? MS_DW4 : MS_DW5;
-MS_DW5:	// fetch data[15:8], wait for rq_ack
-		ms_next = rq_ack ? MS_DW6 : MS_DW5;
-MS_DW6:	// wait for start of WEn, goto DW1
-		ms_next = we ? MS_DW1 : MS_DW6;
-
-MS_DR1:	// wait for rq_ack
-		ms_next = rq_ack ? MS_DR2 : MS_DR1;
-MS_DR2:	// feed data[7:0]
-		ms_next = MS_DR3;
-MS_DR3:	// wait for end of REn
-		ms_next = re ? MS_DR3 : MS_DR4;
-MS_DR4:	// wait for start of REn
-		ms_next = re ? MS_DR5 : MS_DR4;
-MS_DR5:	// feed data[15:8], 	wait for end of REn
-		ms_next = re ? MS_DR5 : MS_DR6;
-MS_DR6:	// wait for start of REn, goto DR1
-		ms_next = re ? MS_DR1 : MS_DR6;
-
-default:
-		ms_next = MS_IDL;
-	endcase
-	
-	
-// states processing
-	always @(posedge clk)
-	case (ms)
-	
-MS_IDL:
-		begin
-			r_rq <=0;
-			w_rq <=0;
-		end
-
-MS_AW2:
-		addr[7:0] <= din;
-MS_AW5:
-		addr[15:8] <= din;
-MS_AW8:
-		addr[23:16] <= din;
-
-MS_DW2:
-		din0[7:0] <= din;
-MS_DW5:
-		w_rq <= 1;
-MS_DW6:
-		begin
-			w_rq <= 0;
-			addr <= addr + 1;
-		end
-
-MS_DR1:
-		r_rq <= 1;
-MS_DR2:
-		begin
-			r_rq <= 0;
-			dout <= d_rd[7:0];
-			addr <= addr + 1;
-		end
-MS_DR4:
-		dout <= d_rd[15:8];
-		
+DR3:
+		if (!re)
+			ms <= ROW;
 		
 	endcase
-	
-	
+	end
+
+
 endmodule
