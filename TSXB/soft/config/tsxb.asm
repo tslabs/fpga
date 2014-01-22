afe924c7c92d6ce2ca185a657c8e3a4d75ccd325
TSL <TSL@localhost>
2014-01-22 17:42:09 +0200

    TSXB: modified FPGA configuration code to use RAM for bitstream transfer

--- TSXB/soft/config/tsxb.asm
+++ TSXB/soft/config/tsxb.asm
@@ -1,36 +1,36 @@
 
-		org $8000
+FP_CTRL	equ $F8AF		; bit0: nCONFIG, bit1: MSEL0, bits6,7: window selection
+FP_STAT	equ $F8AF		; bit0: nSTATUS, bit7: CONF_DONE
 
-		; conf start
-		ld bc, $F0AF
-		ld a, 3
+		org $8000
+		
+		; initiate config
+		ld bc, FP_CTRL
+		ld a, $C3		; nCONFIG = 0, PS MODE, window at C000
 		out (c), a
 		ld b,0
 		djnz $
-		ld bc, $F0AF
-		ld a, 2
+		ld bc, FP_CTRL
+		ld a, $C2		; nCONFIG = 1, PS MODE, window at C000
 		out (c), a
 
-		ld a, 8
-l1:
-		push af
-		ld bc, $13AF
-		out (c), a
-
+		; transfer bitstream
+		ld d, 8
+l1:		ld bc, $13AF
+		out (c), d
+		
+		push de
 		ld hl, $C000
-		ld bc, $EFAF
-cf1:
-		ld a, (hl)
-		out (c), a
-		inc l
-		jp nz, cf1
-		inc h
-		jr nz, cf1
-
-		pop af
-		inc a
-		cp 8+5
-		jr c, l1
+		ld de, $C000
+		ld bc, $4000
+		ldir
+		pop de
+		inc d
+		
+		ld bc, FP_STAT
+		in a, (c)
+		rla
+		jr nc, l1
 
 		jr $
 
